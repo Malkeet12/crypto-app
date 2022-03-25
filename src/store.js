2ea448146170724499f22027be747a61147907af
Malkeet <malkeet@bluestacks.com>
2022-03-25 14:26:29 +0530

    fixing typo

--- src/store.js
+++ src/store.js
@@ -1,9 +1,9 @@
 import { createStore, applyMiddleware } from 'redux';
-import chatReducer from './reducer';
+import reducer from './reducer';
 import thunk from 'redux-thunk';
 import { composeWithDevTools } from 'redux-devtools-extension';
 
 export default createStore(
-  chatReducer,
+  reducer,
   composeWithDevTools(applyMiddleware(thunk))
 );
